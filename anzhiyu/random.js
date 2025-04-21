0bf753e080c056b1e6bb8845e03ea3c1a2a5d230
ZQJ-go <543298913@qq.com>
2025-04-21 13:02:13 +0000

    最新的更新 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/21/first/","2025/04/21/hello-world/","2025/04/21/second/"];function toRandomPost(){
+var posts=["2021/04/06/first/","2025/04/21/hello-world/","2025/04/21/second/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
