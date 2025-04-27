d2e25d71cacbd99931557355fe59e9a3b06168fe
ZQJ-go <543298913@qq.com>
2025-04-27 13:56:18 +0000

    modified:   _config.anzhiyu.yml 	modified:   source/_posts/first.md Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2021/04/06/first/","2025/04/22/hello-world/","2025/04/22/second/"];function toRandomPost(){
+var posts=["2021/04/06/first/","2025/04/27/hello-world/","2025/04/27/second/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
