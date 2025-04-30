46f24bfe5d01f64b530e08de4f261c92243f3d55
ZQJ-go <543298913@qq.com>
2025-04-30 15:44:49 +0000

    new update Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/29/first_passage/","2025/04/29/hello-world/"];function toRandomPost(){
+var posts=["2024/04/29/first_passage/","2024/04/29/game/","2025/04/30/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
